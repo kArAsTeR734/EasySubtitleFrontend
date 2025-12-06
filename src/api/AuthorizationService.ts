eda4bdb32cc8ba9f92ca754843a3834f5f953b1a
kArAsTeR734 <nikita.belyaev.05@mail.ru>
2025-12-06 23:50:01 +0300

    Api fix

--- src/api/AuthorizationService.ts
+++ src/api/AuthorizationService.ts
@@ -29,7 +29,14 @@ export class AuthorizationService {
   }
 
   public static async logout(): Promise<void> {
-    const response = await AuthorizationInstance.post('/api/v1/auth/logout',{},{withCredentials:true});
+    const response = await AuthorizationInstance.post('/api/v1/auth/logout',
+        {},
+        {
+          withCredentials:true,
+          headers:{
+            Authorization: `Bearer ${localStorage.getItem('access_token')}`,
+          }
+        });
     localStorage.removeItem('access_token');
 
     return response.data;
