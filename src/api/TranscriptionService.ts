9b7269068c247188622a513242ff84d0472dce69
kArAsTeR734 <nikita.belyaev.05@mail.ru>
2025-12-06 22:04:35 +0300

    Api fix

--- src/api/TranscriptionService.ts
+++ src/api/TranscriptionService.ts
@@ -1,10 +1,10 @@
-import {TranscriptionInstance} from "./config/api.config.ts";
+import {apiInstance} from "./config/api.config.ts";
 import type {GetAllTranscriptionsResult, TranscriptionResult} from "./types/api-types.ts";
 
 export class TranscriptionService {
 
   public static async getAllTranscriptions(page: number = 1, pageSize: number = 30): Promise<GetAllTranscriptionsResult> {
-    const response = await TranscriptionInstance.get('/api/v1/scripts', {
+    const response = await apiInstance.get('/api/v1/scripts', {
       params: {
         page: page,
         pageSize: pageSize
@@ -14,7 +14,7 @@ export class TranscriptionService {
   }
 
   public static async getTranscriptionResult(id: string): Promise<TranscriptionResult> {
-    const response = await TranscriptionInstance.get(`/api/v1/scripts/${id}`);
+    const response = await apiInstance.get(`/api/v1/scripts/${id}`);
 
     return response.data;
   }
--- src/api/UserService.ts
+++ src/api/UserService.ts
@@ -1,9 +1,9 @@
-import {TranscriptionInstance} from "./config/api.config.ts";
+import {apiInstance} from "./config/api.config.ts";
 import type {UserInfo} from "./types/api-types.ts";
 
 export class UserService {
   public static async getUserInfo(): Promise<UserInfo> {
-    const response = await TranscriptionInstance.get('/api/v1/auth/me')
+    const response = await apiInstance.get('/api/v1/auth/me')
     return response.data;
   }
 }
