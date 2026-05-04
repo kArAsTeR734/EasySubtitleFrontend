65e060a15ea42a38236b712b92169cee4adb14dc
Науменко Дмитрий <AridanWarlock@gmail.com>
2026-05-04 12:55:49 +0300

    fix: request interceptors changed

--- src/features/Logout/useLogout.ts
+++ src/features/Logout/useLogout.ts
@@ -4,6 +4,8 @@ import { AuthorizationService } from '@/api/services/AuthorizationService.ts';
 export const useLogout = () => {
   const queryClient = useQueryClient();
 
+  console.log('token before logout: ', localStorage.getItem('access_token'));
+
   return useMutation({
     mutationKey: ['logout'],
     mutationFn: () => AuthorizationService.logout(),
@@ -12,12 +14,15 @@ export const useLogout = () => {
       queryClient.removeQueries({ queryKey: ['tasks'] });
       queryClient.removeQueries({ queryKey: ['me'] });
       localStorage.removeItem('access_token');
+
+      window.location.reload();
     },
     onError: (error) => {
-      console.log("Произошла ошибка при выходе из аккаунта", error);
+      console.log('Произошла ошибка при выходе из аккаунта', error);
     },
     onSettled: () => {
       console.log('Запрос отправился');
+      console.log('token after logout: ', localStorage.getItem('access_token'));
     }
   });
 };
